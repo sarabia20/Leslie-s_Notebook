7f53da82aa03511b9f65205aad72c8b501d95466
sarabia20 <67522296+sarabia20@users.noreply.github.com>
2020-08-21 02:46:10 -0500

    Update AumentaVida.cs

--- AumentaVida.cs
+++ AumentaVida.cs
@@ -1,9 +1,16 @@
-﻿using System.Collections;
+//Nombre del desarrollador: Sarabia Cruz Sayra Alondra
+//Asignatura: Estructura de datos
+//Descripcion del usos de este codigo:
+/*
+Este script se utilizara para generar el movimiento y desaparición del enemigo. 
+*/
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AumentaVida : MonoBehaviour
-{
+{//Inicio de clase
+    //Area de declaracion*****************************************
     [SerializeField]
     PlayerControl playerPuntaje;
     public int scoreValue;
@@ -12,6 +19,7 @@ public class AumentaVida : MonoBehaviour
     [SerializeField]
     PlayerControl playerVida;
 
+    ****************************************************************
     void Start()
     {              
         playerVida = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControl>();
@@ -29,4 +37,4 @@ public class AumentaVida : MonoBehaviour
             Destroy(this.gameObject);
         }
     }
-}
+}//fin de la clase
