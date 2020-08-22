ede8846179d31d7615b341ce1fc06e8a6be822e4
sarabia20 <67522296+sarabia20@users.noreply.github.com>
2020-08-22 00:43:02 -0500

    Update Crayolas.cs

--- Crayolas.cs
+++ Crayolas.cs
@@ -5,24 +5,24 @@ using UnityEngine;
 public class Crayolas : MonoBehaviour
 {
     [SerializeField]
-    PlayerControl playerPuntaje;
-    public int scoreValue;
+    PlayerControl playerPuntaje;//Manda a llamar el componente del player control que contiene datos sobre que debe mostrar
+    public int scoreValue;//almacenara el dato de tipo int para el score 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        playerPuntaje = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControl>();
+        playerPuntaje = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControl>();//player puntaje es igual a buscar un objeto con el tag Player y a su comoponenete player control
     }
 
      private void OnTriggerEnter2D(Collider2D collision)
     {
 
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player")//si un objeto con el tag player colisona con este objeto
         {
-            playerPuntaje.crayolas++;
-            playerPuntaje.AddScore(scoreValue);
-            Destroy(this.gameObject);
+            playerPuntaje.crayolas++;//suma la vida
+            playerPuntaje.AddScore(scoreValue);//agrega el valor a score
+            Destroy(this.gameObject);//y eliminalo
         }
     }
 }
