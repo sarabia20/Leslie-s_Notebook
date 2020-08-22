083528f85baef18c6de72b3b6c91d82bdaddc8a0
sarabia20 <67522296+sarabia20@users.noreply.github.com>
2020-08-22 03:28:31 -0500

    Update CameraFollowEnemigo.cs

--- CameraFollowEnemigo.cs
+++ CameraFollowEnemigo.cs
@@ -1,4 +1,10 @@
-﻿using System.Collections;
+//Nombre del desarrollador: Sarabia Cruz Sayra Alondra
+//Asignatura: Estructura de datos
+//Descripcion del usos de este codigo:
+/*
+Este script se utilizara para generar el segumiento de la camara del enemigo.
+*/
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,10 +13,8 @@ public class CameraFollowEnemigo : MonoBehaviour
     //Inicio de clase
 
     //+++++++área para declarar variables++++++
-    public Transform target;
-    public Vector3 puntoInicial;
-
-
+    public Transform target;//es una referencia al componente trasnform de un objeto
+    public Vector3 puntoInicial;//es una variable donde se va almacenar la cámara
     //*****************************************
     // Start is called before the first frame update
     void Start()
@@ -21,7 +25,7 @@ public class CameraFollowEnemigo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(Mathf.Clamp(target.position.x, 0f, 0f), Mathf.Clamp(target.position.y, 0f, 44.525f), transform.position.z);
+        transform.position = new Vector3(Mathf.Clamp(target.position.x, 0f, 0f), Mathf.Clamp(target.position.y, 0f, 44.525f), transform.position.z);//es para delimitar hasta que punto puede extender el seguimineto de la cámara en su eje x y
 
     }
-}
+}//Fin de la clase
