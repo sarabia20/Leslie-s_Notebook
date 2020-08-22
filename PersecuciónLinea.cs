b2241b9bdec65d6b71fa8e57e47d5f3479284567
sarabia20 <67522296+sarabia20@users.noreply.github.com>
2020-08-22 02:25:45 -0500

    Update PersecuciónLinea.cs

--- PersecuciónLinea.cs
+++ PersecuciónLinea.cs
@@ -12,16 +12,16 @@ public class PersecuciónLinea : MonoBehaviour
 {//Inicio de clase
 
     //+++++++área para declarar variables++++++
-    public Transform target; 
+    public Transform target; //es la variable que guarda el objetivo a persegir
     // Start is called before the first frame update
     void Start()
     {
-        target = GameObject.FindGameObjectWithTag("Objetivo").GetComponent<Transform>();
+        target = GameObject.FindGameObjectWithTag("Objetivo").GetComponent<Transform>();//el target a buscar/perseguir es el objeto con el tag denominado "player" asi se genera asignacion automatica del target
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, target.position, 0.4f*Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, target.position, 0.4f*Time.deltaTime);//es el limite de movimiento que podra tener la plataforma
     }
 }//Fin de la clase
